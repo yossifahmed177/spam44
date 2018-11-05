92dbb489b51c7cd54512fc79097c683ce2a3d89b
yossifahmed177 <44752698+yossifahmed177@users.noreply.github.com>
2018-11-05 12:06:22 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# spam44
+# Fuckbossishere
---
+++ bot.js
@@ -0,0 +1,64 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("508937856597884928")
+setInterval(function() {
+channel.send(`:rose: الله و اكبر 1`);
+channel.send(`:rose: الله و اكبر 2`);
+channel.send(`:rose: الله و اكبر 3`);
+channel.send(`:rose: الله و اكبر 4`);
+channel.send(`:rose: الله و اكبر 5`);
+channel.send(`:rose: الله و اكبر 6`);
+channel.send(`:rose: الله و اكبر 7`);
+channel.send(`:rose: الله و اكبر 8`);
+channel.send(`:rose: الله و اكبر 9`);
+channel.send(`:rose: الله و اكبر 10`);
+channel.send(`:rose: الله و اكبر 11`);
+channel.send(`:rose: الله و اكبر 12`);
+channel.send(`:rose: الله و اكبر 13`);
+channel.send(`:rose: الله و اكبر 14`);
+channel.send(`:rose: الله و اكبر 15`);
+channel.send(`:rose: الله و اكبر 16`);
+channel.send(`:rose: الله و اكبر 17`);
+channel.send(`:rose: الله و اكبر 18`);
+channel.send(`:rose: الله و اكبر 19`);
+channel.send(`:rose: الله و اكبر 20`);
+channel.send(`:rose: الله و اكبر 21`);
+channel.send(`:rose: الله و اكبر 22`);
+channel.send(`:rose: الله و اكبر 23`);
+channel.send(`:rose: الله و اكبر 24`);
+channel.send(`:rose: الله و اكبر 25`);
+channel.send(`:rose: الله و اكبر 26`);
+channel.send(`:rose: الله و اكبر 27`);
+channel.send(`:rose: الله و اكبر 28`);
+channel.send(`:rose: الله و اكبر 29`);
+channel.send(`:rose: الله و اكبر 30`);
+channel.send(`:rose: الله و اكبر 31`);
+channel.send(`:rose: الله و اكبر 32`);
+channel.send(`:rose: الله و اكبر 33`);
+channel.send(`:rose: الله و اكبر 34`);
+channel.send(`:rose: الله و اكبر 35`);
+channel.send(`:rose: الله و اكبر 36`);
+channel.send(`:rose: الله و اكبر 37`);
+channel.send(`:rose: الله و اكبر 38`);
+channel.send(`:rose: الله و اكبر 39`);
+channel.send(`:rose: الله و اكبر 40`);
+channel.send(`:rose: الله و اكبر 41`);
+channel.send(`:rose: الله و اكبر 42`);
+channel.send(`:rose: الله و اكبر 43`);
+channel.send(`:rose: الله و اكبر 44`);
+channel.send(`:rose: الله و اكبر 45`);
+channel.send(`:rose: الله و اكبر 46`);
+channel.send(`:rose: الله و اكبر 47`);
+channel.send(`:rose: الله و اكبر 48`);
+channel.send(`:rose: الله و اكبر 49`);
+channel.send(`:rose: الله و اكبر 50`);
+
+}, 2500)
+})
+ 
+ 
+client.login("NTA0NDM2MDExMjYzMzI4MjU3.DsGl9Q.YOQ2ute23J6E-LQwzfjocDPzdXg");
